038cf5c30c624d5fbc1d2422ebbe6312b329e77c
erandaka.bandara <erandakaanupama@gmail.com>
2024-03-10 07:48:24 +0530

    chore: init commit

---
+++ src/main/java/com/eab/myspaceeurekaserver/MyspaceEurekaserverApplication.java
@@ -0,0 +1,13 @@
+package com.eab.myspaceeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspaceEurekaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyspaceEurekaserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eab/myspaceeurekaserver/MyspaceEurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eab.myspaceeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyspaceEurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
